0a7315dc0e1d081841704ce40939a44e5324a9c9
gvalenciam <gvalenciam@pucp.pe>
2021-02-14 21:03:08 -0500

    First commit. Home page 50%. Falta hacer componente de navbar

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import utilStyles from "../styles/utils.module.scss";
+import cs from "classnames";
+
+import styles from "../styles/pages/Inicio.module.scss";
 
 export default function Home() {
   return (
@@ -9,57 +13,52 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <section className={styles.seccionBienvenida}>
+        <Image
+          src={"/images/Inicio/background.png"}
+          alt="Background inicio"
+          layout="fill"
+        ></Image>
+        <div className={cs(utilStyles.contenedorTituloSubtituloBackground)}>
+          <h1 className={utilStyles.tituloBackground}>
+            Ríos danzantes en la cuenca amazónica
+          </h1>
+          <p className={utilStyles.subtituloBackground}>
+            La ciencia detrás del movimiento de los ríos
+          </p>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+        <div
+          className={cs(styles.contenedorLogos, utilStyles.flexBetweenCenter)}
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+          <div className={styles.logo}>
+            <Image
+              src="/images/Inicio/Logo_utec_blanco.png"
+              alt="Logo UTEC"
+              height={141}
+              width={298}
+              layout="responsive"
+            ></Image>
+          </div>
+          <div className={styles.logo}>
+            <Image
+              src="/images/Inicio/Logo_moore_blanco.png"
+              alt="Logo UTEC"
+              height={173}
+              width={444}
+              layout="responsive"
+            ></Image>
+          </div>
+          <div className={styles.logo}>
+            <Image
+              src="/images/Inicio/Logo_cita_blanco.png"
+              alt="Logo UTEC"
+              height={200}
+              width={592}
+              layout="responsive"
+            ></Image>
+          </div>
+        </div>
+      </section>
     </div>
-  )
+  );
 }
